5ad24fbe68090ce6b0483c10d9d8596a3764040b
Aidan <acm289@cornell.edu>
2025-04-28 14:24:36 -0400

    Start website

---
+++ docs/conf.py
@@ -0,0 +1,26 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "ECE 5730: Clocktower LEDs"
+copyright = "2025, Aidan McNay, Edmund Lam, Qingmiao Xiao"
+author = "Aidan McNay, Edmund Lam, Qingmiao Xiao"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
