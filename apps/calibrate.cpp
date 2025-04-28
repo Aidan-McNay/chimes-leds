d6438f93fc911d2437f88a1cf4414e9060829722
UnsignedByte <edlam1889@gmail.com>
2025-04-28 14:19:12 -0400

    fixed calibration software

--- apps/calibrate.cpp
+++ apps/calibrate.cpp
@@ -28,52 +28,62 @@ color_rgb balanced = {128, 128, 128};
 static PT_THREAD( protothread_input( struct pt *pt ) )
 {
   PT_BEGIN( pt );
-  static int begin_time;
-
   while (1) {
     sprintf(pt_serial_out_buffer, "Enter new RGB values (r, g, b): ");
     serial_write;
     serial_read;
     // Read %d, %d, %d as the new RGB values
-    if (!sscanf(pt_serial_in_buffer, "%d, %d, %d", &balanced.red, &balanced.green, &balanced.blue)) {
-        // If the input is invalid, print an error message
-        sprintf(pt_serial_out_buffer, "Invalid input. Please enter three integers separated by commas.\n");
-        serial_write;
+    int r, g, b;
+    if (!sscanf(pt_serial_in_buffer, "%d, %d, %d", &r, &g, &b)) {
+      sprintf(pt_serial_out_buffer, "Invalid input. Please enter three integers separated by commas.\n");
+      serial_write;
+    } else {
+      // If the input is invalid, print an error message
+      balanced.red = r;
+      balanced.green = g;
+      balanced.blue = b;
+      sprintf(pt_serial_out_buffer, "New RGB values: %d, %d, %d\n", balanced.red, balanced.green, balanced.blue);
+      serial_write;
     }
+
   }
   PT_END( pt );
 }
 
 static PT_THREAD( protothread_blink (struct pt *pt) )
 {
-    PT_BEGIN( pt );
-    
-    while (1) {
-        // Blink the LED
-        led.set_rgbw(balanced.red, balanced.green, balanced.blue, 0);
-        PT_YIELD_usec(500000);
-        led.set_rgbw(0, 0, 0, 128);
-        PT_YIELD_usec(500000);
-    }
-    PT_END( pt );
+  PT_BEGIN( pt );
+  
+  while (1) {
+    // Blink the LED
+    led.set_rgbw(balanced.red, balanced.green, balanced.blue, 0);
+    PT_YIELD_usec(500000);
+    led.set_rgbw(0, 0, 0, 128);
+    PT_YIELD_usec(500000);
+  }
+  PT_END( pt );
 }
 
 // Main for core 0
 int main() {
-    // Initialize stdio
-    stdio_init_all();
+  // Initialize stdio
+  stdio_init_all();
 
-    gpio_set_function( 0, UART_FUNCSEL_NUM( uart0, 0 ) );
-    gpio_set_function( 1, UART_FUNCSEL_NUM( uart0, 1 ) );
+  gpio_init( PICO_DEFAULT_LED_PIN );
+  gpio_set_dir( PICO_DEFAULT_LED_PIN, GPIO_OUT );
+  gpio_put( PICO_DEFAULT_LED_PIN, 1 );
 
-    uart_init( uart0, 115200 );
+  gpio_set_function( 0, UART_FUNCSEL_NUM( uart0, 0 ) );
+  gpio_set_function( 1, UART_FUNCSEL_NUM( uart0, 1 ) );
 
-    uart_puts( uart0, "Initialized calibration.\n" );
+  uart_init( uart0, 115200 );
 
-    led.toggle(true);
+  uart_puts( uart0, "Initialized calibration.\n" );
 
-    pt_add_thread(protothread_input);
-    pt_add_thread(protothread_blink);
-    
-    pt_schedule_start ;
+  led.toggle(true);
+
+  pt_add_thread(protothread_input);
+  pt_add_thread(protothread_blink);
+  
+  pt_schedule_start ;
 }
